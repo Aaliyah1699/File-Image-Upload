152b70699e416c84ef0f0dcedcce1f730d9addd4
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-23 18:51:54 -0500

    check - if file exist, format, and size errors

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,23 @@
 const path = require("path");
 const { StatusCodes } = require("http-status-codes");
+const customError = require("../errors");
 
 const uploadProductImage = async (req, res) => {
+    // check - if file exist, format, and size
+    if (!req.files) {
+        throw new customError.BadRequestError("No File Uploaded");
+    }
     const productImage = req.files.image;
+
+    if (!productImage.mimetype.startsWith("image")) {
+        throw new customError.BadRequestError("Please Upload Image");
+    }
+    const maxSize = 1024 * 1024; 
+    if (productImage.size > maxSize) {
+        throw new customError.BadRequestError(
+            "Please Upload Image Smaller 1MB"
+        );
+    }
 
     const imagePath = path.join(
         __dirname,
@@ -13,7 +28,6 @@ const uploadProductImage = async (req, res) => {
     return res
         .status(StatusCodes.OK)
         .json({ image: { src: `/uploads/${productImage.name}` } });
-
 };
 
 module.exports = { uploadProductImage };
