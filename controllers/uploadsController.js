8f603ef099c0e2aaecb1ec0c1a732b8f1c90efdf
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-23 19:46:29 -0500

    refactor to upload images to the cloud

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,22 @@
 const path = require("path");
 const { StatusCodes } = require("http-status-codes");
 const customError = require("../errors");
+const cloudinary = require("cloudinary").v2;
 
 const uploadProductImage = async (req, res) => {
+    const result = await cloudinary.uploader.upload(
+        req.files.image.tempFilePath,
+        {
+            use_filename: true,
+            folder: "Files and Image upload api",
+        }
+    );
+    return res
+        .status(StatusCodes.OK)
+        .json({ image: { src: result.secure_url } });
+};
+
+const uploadProductImageLocal = async (req, res) => {
     // check - if file exist, format, and size
     if (!req.files) {
         throw new customError.BadRequestError("No File Uploaded");
@@ -12,7 +26,7 @@ const uploadProductImage = async (req, res) => {
     if (!productImage.mimetype.startsWith("image")) {
         throw new customError.BadRequestError("Please Upload Image");
     }
-    const maxSize = 1024 * 1024; 
+    const maxSize = 1024 * 1024;
     if (productImage.size > maxSize) {
         throw new customError.BadRequestError(
             "Please Upload Image Smaller 1MB"
